1db7d8f3fa7128f85188b96856112f6c6879fc5d
Qosimjon Omonov <qosimjonomonovko@gmail.com>
2026-04-09 21:32:30 +0500

    feat: register added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import { BrowserRouter, useNavigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes/routes";
-import { useEffectEvent } from "react";
+
 
 function App() {
-  // const navigate = useNavigate();
 
-  // useEffectEvent(() => {
-  //   const user = localStorage.getItem("user");
-  //   if (!user) navigate("/register");
-  // }, []);
   return (
     <BrowserRouter>
       <AppRouter />
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../pages/home/Home";
 import { Catalog } from "../pages/Catalog/Catalog";
 import { Search } from "../pages/Search/Search";
@@ -10,17 +10,30 @@ import { Collection } from "../pages/Collection/Collection";
 import { Register } from "../pages/Register";
 
 export const AppRouter = () => {
+  const user = JSON.parse(localStorage.getItem("user") || "null");
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/movie/:id" element={<MovieDetail />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/my" element={<MyMovie />} />
-      <Route path="/person/:id" element={<Person />} />
-      <Route path="/collections" element={<Collection />} />
-      <Route path="/register" element={<Register />} />
+      {!user ? (
+        <>
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/register" replace />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/my" element={<MyMovie />} />
+          <Route path="/person/:id" element={<Person />} />
+          <Route path="/collections" element={<Collection />} />
+
+          <Route path="/register" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </>
+      )}
     </Routes>
   );
 };
